9889c42e73b5797b8e055dfe6cd2c570ed8014d0
Riva Junior <jrcavalcantejr@gmail.com>
2023-03-05 19:05:01 -0300

    build: add rollup-plugin-gzip
    This plugin will be used to compress the production bundle using brotli

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import gzipPlugin from 'rollup-plugin-gzip';
+import { brotliCompress } from 'node:zlib';
+import { promisify } from 'node:util';
+
+const brotliPromise = promisify(brotliCompress);
 
 export default defineConfig({
-	plugins: [solidPlugin()],
+	plugins: [
+		solidPlugin(),
+		{
+			...gzipPlugin({
+				customCompression: async (content) =>
+					brotliPromise(Buffer.from(content)),
+				fileName: '.br',
+			}),
+			enforce: 'post',
+			apply: 'build',
+		},
+	],
 	server: {
 		port: 3000,
 	},
